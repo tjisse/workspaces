33d4802cb249beb3549323bd60122cd10af298ea
Tony Kay <tony.kay@gmail.com>
2021-01-04 13:54:01 -0800

    shadow fix 2.9+

--- src/nubank/workspaces/shadow_cljs/target.clj
+++ src/nubank/workspaces/shadow_cljs/target.clj
@@ -4,47 +4,46 @@
     [shadow.build :as build]
     [shadow.build.modules :as modules]
     [shadow.build.targets.browser :as browser]
-    [shadow.build.classpath :as cp]
     [shadow.build.test-util :as tu]))
 
 ;; Mostly taken from shadow-cljs :browser-test target type.
 (defn modify-config [{::build/keys [config] :as state}]
   (-> state
-    (assoc-in [::build/config :modules :main] {:entries '[nubank.workspaces.shadow-cljs.mount]})
-    (assoc-in [::build/config :compiler-options :source-map] true) ;; always
-    (assoc-in [::build/config :compiler-options :static-fns] false) ;; for mocking
-    (update :build-options merge {:greedy          true
-                                  :dynamic-resolve true})
-    (update-in [::build/config :devtools] merge
-      {:before-load 'nubank.workspaces.core/before-load
-       :after-load  'nubank.workspaces.core/after-load})))
+      (assoc-in [::build/config :modules :main] {:entries '[nubank.workspaces.shadow-cljs.mount]})
+      (assoc-in [::build/config :compiler-options :source-map] true) ;; always
+      (assoc-in [::build/config :compiler-options :static-fns] false) ;; for mocking
+      (update :build-options merge {:greedy          true
+                                    :dynamic-resolve true})
+      (update-in [::build/config :devtools] merge
+        {:before-load 'nubank.workspaces.core/before-load
+         :after-load  'nubank.workspaces.core/after-load})))
 
 (defn resolve-cards-and-tests
   [{::build/keys [mode config] :as state}]
   (let [dynamically-resolved-namespaces (tu/find-test-namespaces state config)]
     (-> state
-      ;; Add the mounter and all of the resolved cards/tests
-      (assoc-in [::modules/config :main :entries] (-> []
-                                                      (into (get config :preloads []))
-                                                      (into dynamically-resolved-namespaces)
-                                                      (conj 'nubank.workspaces.shadow-cljs.mount)))
-      (cond->
-        (and (= :dev mode) (:worker-info state))
-        (update-in [::modules/config :main] browser/inject-repl-client state config)
+        ;; Add the mounter and all of the resolved cards/tests
+        (assoc-in [::modules/config :main :entries] (-> []
+                                                        (into (get config :preloads []))
+                                                        (into dynamically-resolved-namespaces)
+                                                        (conj 'nubank.workspaces.shadow-cljs.mount)))
+        (cond->
+          (and (= :dev mode) (:worker-info state))
+          (update-in [::modules/config :main] browser/inject-repl-client state config)
 
-        (= :dev mode)
-        (-> (update-in [::modules/config :main] browser/inject-preloads state config)
-          (update-in [::modules/config :main] browser/inject-devtools-console state config)))
-      (modules/analyze))))
+          (= :dev mode)
+          (-> (update-in [::modules/config :main] browser/inject-preloads state config)
+              (update-in [::modules/config :main] browser/inject-devtools-console state config)))
+        (modules/analyze))))
 
 (defn process
   [{::build/keys [stage] :as state}]
   (-> state
-    (cond->
-      (= :configure stage)
-      (modify-config)
+      (cond->
+        (= :configure stage)
+        (modify-config)
 
-      (= :resolve stage)
-      (resolve-cards-and-tests))
+        (= :resolve stage)
+        (resolve-cards-and-tests))
 
-    (browser/process)))
+      (browser/process)))
