faa85e9a0afa9c9d8a45832e08cc3660cbc93ea4
Tjisse van der Wal <tjissevdwal@gmail.com>
2024-07-06 14:34:15 +0200

    Use highlightElement instead of highlightBlock

--- src/nubank/workspaces/ui/highlight.cljs
+++ src/nubank/workspaces/ui/highlight.cljs
@@ -1,12 +1,12 @@
 (ns nubank.workspaces.ui.highlight
   (:require [com.fulcrologic.fulcro.components :as fp]
             [com.fulcrologic.fulcro-css.localized-dom :as dom]
-            ["highlight.js" :refer [highlightBlock]]))
+            ["highlight.js" :refer [highlightElement]]))
 
 (fp/defsc Highlight [this {::keys [source language]}]
   {:componentDidMount
    (fn [this]
-     (highlightBlock (dom/node this)))}
+     (highlightElement (dom/node this)))}
 
   (dom/pre {:className (or language "clojure")} source))
 
